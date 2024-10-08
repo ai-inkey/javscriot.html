b0da08616984d4e8df239332de40937d04eea748
ai-inkey <bdteamzzz@gmail.com>
2024-10-08 18:51:38 +0600

    rr

---
+++ htmldom-acp.js
@@ -0,0 +1,5 @@
+
+function myfunction(){
+    var a= 4;
+    document.getElementById("js").innerHTML = a*a;
+}
--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 function validate(e){
-    e.preventDeafault();
+    e.preventDefault();
 
 const email= document.getElementById("email").value;
 const password=document.getElementById("password").value;
 const age=document.getElementById("age").value;
-const msgbox=document.getElementById("massage").value;
+const msgBox=document.getElementById("message");
 
-let massage='';
+let message='';
 if(email===''){
-    massage='mail is required'
-    msgbox.style.color='red';
+    message='mail is required'
+    msgBox.style.color='red';
 }
 else if(password===''){
-    massage='password must be at least 8 characters'
-    msgbox.style.color='red'
+    message='password must be at least 8 characters'
+    msgBox.style.color='red'
 }
 else if(age===''){
-    massage='age must be between 12-50'
-    msgbox.style.color='red'
+    message='age must be between 12-50'
+    msgBox.style.color='red'
 
 }
-else{massage='Information is valid'
-    msgbox.bo.style.color='green'
+else{message='Login Successfull'
+    msgBox.style.color='green'
 }
 
-msgbox.innertext=massage;
+msgBox.innerText=message;
 }
 
 
